12d59d7172f7883d375cd79faae6e4f092183370
AlexWSN <sasualexandra97@gmail.com>
2024-09-07 12:29:29 +0300

    commit

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function createParticle(type) {
-    const particle = document.createElement('div');
-    particle.classList.add(type);
-    particle.style.left = `${Math.random() * 100}vw`;
-    particle.style.animationDuration = `${Math.random() * 3 + 2}s`;
+  const particle = document.createElement("div");
+  particle.classList.add(type);
+  particle.style.left = `${Math.random() * 100}vw`;
+  particle.style.animationDuration = `${Math.random() * 3 + 2}s`;
 
-    document.querySelector('.background').appendChild(particle);
+  document.querySelector(".background").appendChild(particle);
 
-    // Eliminăm particula după terminarea animației
-    setTimeout(() => {
-        particle.remove();
-    }, 5000);
+  // Eliminăm particula după terminarea animației
+  setTimeout(() => {
+    particle.remove();
+  }, 5000);
 }
 
 // Generăm scântei și fulgi de cenușă continuu
 setInterval(() => {
-    createParticle('spark');
-    createParticle('ash');
+  createParticle("spark");
+  createParticle("ash");
 }, 200);
